f821bab329e363d8266a52ec64f9ccf7de0d65df
Sagar-1608 <jadhavss1608@gmail.com>
2023-07-16 23:36:22 +0530

    Mail function added in code succesfully

---
+++ 13_File_Handeling/lect_1_File_Upload/config/mailer.js
@@ -0,0 +1,18 @@
+const nodemailer = require("nodemailer")
+
+require("dotenv").config();
+
+
+ const mailer = nodemailer.createTransport({
+        host:process.env.MAIL_HOST,
+        auth:{
+            user:process.env.MAIL_USER,
+            pass:process.env.MAIL_PASS
+        }
+
+    })
+
+module.exports= mailer;
+
+
+ 
--- 13_File_Handeling/lect_1_File_Upload/models/File.js
+++ 13_File_Handeling/lect_1_File_Upload/models/File.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const mailer = require("../config/mailer");
+const nodemailer = require("nodemailer")
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -15,6 +17,37 @@ const fileSchema = new mongoose.Schema({
         type:String,
     }
 });
+
+// to send the mail TO when new entry sane in DB 
+// this write after the module export 
+fileSchema.post("save",async function(doc){
+
+ try{ 
+    
+    const nodemailerInstance =mailer;
+    console.log(doc);
+    
+   let info= await nodemailerInstance.sendMail({
+        from:"FileUploader_$J",
+        to:doc.email,
+        subject:"Your file uploaded on Cloudinary",
+        html:`<h2>Hello ${doc.email} </h2> <p>File Uploaded View Here: <a href="${doc.imageUrl}"> ${doc.imageUrl}></a> </p>`
+    })
+
+
+
+    console.log(info)
+
+
+ }
+ catch(err){
+
+    console.error(err);
+ }
+    
+
+
+})
 
 const File = mongoose.model("File", fileSchema);
 module.exports = File;
